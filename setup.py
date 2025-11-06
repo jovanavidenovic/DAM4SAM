1ba044e9627015eb382364d821d41a95821d84fd
Tomáš Čížek <99816196+Ciza12@users.noreply.github.com>
2025-11-06 15:15:21 +0100

    Rename package from SAM-2 to DAM4SAM

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os
 from setuptools import find_packages, setup
 
 # Package metadata
-NAME = "SAM-2"
+NAME = "DAM4SAM"
 VERSION = "1.0"
 DESCRIPTION = "SAM 2: Segment Anything in Images and Videos"
 URL = "https://github.com/facebookresearch/sam2"
